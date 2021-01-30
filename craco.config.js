f9eb7998f7c01b16593690fd3fa25c55868018e0
Tuzilow <1176281967@qq.com>
2021-01-30 12:01:38 +0800

    github pages自动部署

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,16 @@
 module.exports = {
   style: {
     postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
+      plugins: [require('tailwindcss'), require('autoprefixer')],
     },
   },
-}
+  webpack: {
+    configure: (webpackConfig) => {
+      webpackConfig.output = {
+        ...webpackConfig.output,
+        publicPath: '/team-h5',
+      };
+      return webpackConfig;
+    },
+  },
+};
